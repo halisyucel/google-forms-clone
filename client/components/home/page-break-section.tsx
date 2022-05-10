5df00093e136f02a24406375ab7087ad9bf5f27b
halisycel <isaycel2001@gmail.com>
2022-05-10 13:43:39 +0300

    🍬 add page-break section to home page

---
+++ client/components/home/page-break-section.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import styles from '../../styles/components/home.page-break-section.module.scss';
+
+const PageBreakSection = () => {
+	return (
+		<section className={styles.page_break_section}>
+			<a href={'#features'} className={styles.page_break_section__content}>
+				<span className={styles.page_break_section__content__text}>See what you can do with Google Forms</span>
+				<ArrowBackIosIcon className={styles.page_break_section__content__svg}/>
+			</a>
+			<img
+				className={styles.page_break_section__background_image}
+				src={'/background-image-1.png'}
+				alt={'Google Forms Clone'}
+			/>
+		</section>
+	);
+};
+
+export default PageBreakSection;
--- client/components/home/page-content.tsx
+++ client/components/home/page-content.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import OverviewSection from './overview-section';
 import styles from '../../styles/components/home.page-content.module.scss';
+import PageBreakSection from './page-break-section';
 
 const PageContent = () => {
 	return (
 		<div className={styles.page_content}>
 			<OverviewSection />
+			<PageBreakSection />
 		</div>
 	);
 };
