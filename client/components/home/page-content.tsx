99eef8e4be7b58da7e99524405923c123105b86d
halisycel <isaycel2001@gmail.com>
2022-05-10 15:17:55 +0300

    🍦 add feature section to home page

--- client/components/home/page-content.tsx
+++ client/components/home/page-content.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import OverviewSection from './overview-section';
-import styles from '../../styles/components/home.page-content.module.scss';
 import PageBreakSection from './page-break-section';
+import FeaturesSection from './features-section';
+import styles from '../../styles/components/home.page-content.module.scss';
 
 const PageContent = () => {
 	return (
 		<div className={styles.page_content}>
 			<OverviewSection />
 			<PageBreakSection />
+			<FeaturesSection />
 		</div>
 	);
 };
