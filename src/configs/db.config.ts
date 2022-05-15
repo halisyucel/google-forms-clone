c25066b52903ef2fd1245d11400ccb2f7b8c9390
halisycel <isaycel2001@gmail.com>
2022-05-15 15:13:23 +0300

    🧑‍💻 fix config files

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -2,7 +2,7 @@ import GeneralConfig from './general.config';
 
 type DataBaseConfig = {
   host: string;
-  port: number;
+  port: string;
   user: string;
   password: string;
   database: string;
@@ -10,7 +10,7 @@ type DataBaseConfig = {
 
 const db: DataBaseConfig = {
 	host: GeneralConfig.DB_HOST,
-	port: GeneralConfig.DB_PORT || 3306,
+	port: GeneralConfig.DB_PORT,
 	user: GeneralConfig.DB_USER,
 	password: GeneralConfig.DB_PASSWORD,
 	database: GeneralConfig.DB_NAME,
--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -2,31 +2,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-type Config = {
-	PORT: number,
+interface Config {
+	PORT: string,
 	JWT_SECRET: string,
 	DB_HOST: string,
 	DB_USER: string,
 	DB_PASSWORD: string,
 	DB_NAME: string,
-	DB_PORT: number,
+	DB_PORT: string,
 }
 
 const config: Config = {
-	// @ts-ignore
-	PORT: process.env.PORT || 3001,
-	// @ts-ignore
-	JWT_SECRET: process.env.JWT_SECRET,
-	// @ts-ignore
-	DB_HOST: process.env.DB_HOST,
-	// @ts-ignore
-	DB_USER: process.env.DB_USER,
-	// @ts-ignore
-	DB_PASSWORD: process.env.DB_PASSWORD,
-	// @ts-ignore
-	DB_NAME: process.env.DB_NAME,
-	// @ts-ignore
-	DB_PORT: process.env.DB_PORT,
+	PORT: process.env.PORT as string,
+	JWT_SECRET: process.env.JWT_SECRET as string,
+	DB_HOST: process.env.DB_HOST as string,
+	DB_USER: process.env.DB_USER as string,
+	DB_PASSWORD: process.env.DB_PASSWORD as string,
+	DB_NAME: process.env.DB_NAME as string,
+	DB_PORT: process.env.DB_PORT as string,
 };
 
 export default config;
