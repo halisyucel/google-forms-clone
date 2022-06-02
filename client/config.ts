7903a0cb2b9434e75202a10e06a923f9c1e8f7b7
halisycel <isaycel2001@gmail.com>
2022-06-03 00:48:34 +0300

    some configuration

--- client/config.ts
+++ client/config.ts
@@ -1,12 +1,16 @@
 interface Config {
 	MODE: 'development' | 'production';
 	API_URL: string;
+	UPLOADS_URL: string;
 }
 
 const config: Config = {
 	MODE: 'development',
 	get API_URL() {
 		return this.MODE === 'development' ? 'http://localhost:3001/api' : '/api';
+	},
+	get UPLOADS_URL() {
+		return this.MODE === 'development' ? 'http://localhost:3001/uploads' : '/uploads';
 	},
 };
 
